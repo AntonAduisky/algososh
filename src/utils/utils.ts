7a3c06c5c3bdbdd657579e7cb2862d84cf4be91a
AntonAduisky <a.mltw@yandex.ru>
2022-10-23 13:12:31 +0300

    feat: delete unused utils functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,3 @@
-export function setUniqueId(): number {
-  return Date.now() * Math.random()
-};
-
 export const setDelay = (ms: number) => {
   return new Promise((res) => setTimeout(res, ms));
 };
