f3f969ca24bfd4d29d9efeaf8c8b3cf1bd7063c2
AntonAduisky <a.mltw@yandex.ru>
2022-11-06 12:43:23 +0300

    feat: comments in btn test

--- src/components/ui/button/button.test.jsx
+++ src/components/ui/button/button.test.jsx
@@ -1,45 +1,43 @@
-import React from "react";
 import renderer from 'react-test-renderer';
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent } from '@testing-library/react';
 
-import { Button } from "./button";
+import { Button } from './button';
 
-describe("Check props button", () => {
-  it("Button props text", () => {
-    const tree = renderer.create(<Button text={"Test"} />).toJSON();
-    expect(tree).toMatchSnapshot(); // для генерации снепшота
+describe('Check props button', () => {
+  it('Button component text-props are displayed correctly', () => {
+    // рендерим Button с текстом 
+    const tree = renderer.create(<Button text={'test text'} />).toJSON();
+    // проверяепм 
+    expect(tree).toMatchSnapshot();
   });
-
-  it("button no text", () => {
+//Функция it — это сокращенное имя для функции test.
+  it('Button component are displayed correctly without props', () => {
     const tree = renderer.create(<Button />).toJSON();
-    expect(tree).toMatchSnapshot(); // для генерации снепшота
+    // expect дает вам доступ к ряду «сопоставителей», которые позволяют вам проверять разные вещи
+    //toMatchSnapshot() сравнивает два снимка: ожидаемый (который был ранее записан в tree) и актуальный (который получился при текущем выполнении теста).
+    expect(tree).toMatchSnapshot();
   });
 
-  it("button disabled", () => {
+  it('Button component disabled correctly', () => {
     const tree = renderer.create(<Button disabled />).toJSON();
-    expect(tree).toMatchSnapshot(); // для генерации снепшота
+    expect(tree).toMatchSnapshot();
   });
 
-  it("button isLoading", () => {
+  it('Button component loading-props are displayed correctly', () => {
     const tree = renderer.create(<Button isLoader />).toJSON();
-    expect(tree).toMatchSnapshot(); // для генерации снепшота
+    expect(tree).toMatchSnapshot();
   });
-  
-  it('button callback', () => {
-    
+
+  it('Button component onClick-props are displayed correctly', () => {
+    //Имитация браузерного alert
     window.alert = jest.fn();
-
-        // Рендерим компонент
-    render(<Button onClick={()=> window.alert('text')} text={'test text'}/>)
-
-        // Находим элемент ссылки
-      // eslint-disable-next-line no-restricted-globals
-      const button = screen.getByText('test text');
-
-        // Имитируем нажатие на ссылку
+    // Рендерим компонент
+    render(<Button onClick={() => window.alert('text')} text={'test text'} />)
+    // Находим элемент ссылки
+    const button = screen.getByText('test text');
+    // Имитируем нажатие на ссылку
     fireEvent.click(button);
-        
-        // Проверяем, что alert сработал с правильным текстом предупреждения
+    // Проверяем, что alert сработал с правильным текстом предупреждения
     expect(window.alert).toHaveBeenCalledWith('text');
-})
+  })
 });
