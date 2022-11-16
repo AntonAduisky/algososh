acc2e281f42f3d90b56fac99c9c13cbeddbb362a
AntonAduisky <a.mltw@yandex.ru>
2022-11-16 23:52:11 +0300

    feat: available and routing test's

--- cypress/e2e/is-available.spec.cy.js
+++ cypress/e2e/is-available.spec.cy.js
@@ -1,5 +1,5 @@
-describe('checking application startup and routing', function() {
-  it('application launched', function() {
-    cy.visit('/');
+describe('Application is available', function() {
+  it('Should be available on localhost:3000', function() {
+    cy.visit('http://localhost:3000');
   });
-});
+}); 
